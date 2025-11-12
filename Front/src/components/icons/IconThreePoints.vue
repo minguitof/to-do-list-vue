b9b205b379aa85830c59e73ddf99b4180539bd92
minguitof <jhonechavarria0506@gmail.com>
2025-11-12 00:35:25 -0500

    Add: Se añade nuevos componentes dark a las secciones

--- Front/src/components/icons/IconThreePoints.vue
+++ Front/src/components/icons/IconThreePoints.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" class="svg-puntos-menu">
     <circle cx="5" cy="12" r="2" />
     <circle cx="12" cy="12" r="2" />
     <circle cx="19" cy="12" r="2" />
